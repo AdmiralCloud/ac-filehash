a93ec762cf036315fe669d24ee9a68981aab8e74
Mark Poepping <mark.poepping@admiralcloud.com>
2023-07-21 15:04:19 +0200

    test(App): Updated tests to use mocha/chai | MP
    Removed expect and use mocha/chai
    Related issues:
    - https://github.com//issues/undefined

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const expect = require('expect')
+const { expect } = require('chai')
 const acfh = require('../index')
 const acfhBrowser = require('../browser')
 
@@ -9,60 +9,60 @@ const expectedFileSize = 158008374
 describe('Compare values', () => {
   it('Test local file', async () => {
     let test = await acfh.getHash({ filePath: './test/testfile.mp4' })
-    expect(test.type).toEqual('file')
-    expect(test.hash).toEqual(expectedHash)
-    expect(test.fileSize).toEqual(expectedFileSize)
+    expect(test.type).to.eql('file')
+    expect(test.hash).to.eql(expectedHash)
+    expect(test.fileSize).to.eql(expectedFileSize)
   })
 
   it('Test URL', async () => {
     let test = await acfh.getHash({ url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' })
-    expect(test.type).toEqual('url')
-    expect(test.hash).toEqual(expectedHash)
-    expect(test.fileSize).toEqual(expectedFileSize)
+    expect(test.type).to.eql('url')
+    expect(test.hash).to.eql(expectedHash)
+    expect(test.fileSize).to.eql(expectedFileSize)
   })
 
   it('Test non existing URL', async () => {
     let test = await acfh.getHash({ url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp3' })
-    expect(test.error).toEqual('invalidURL')
+    expect(test.error).to.eql('invalidURL')
   })
 
   it('Test non existing local file', async () => {
     let test = await acfh.getHash({ filePath: './test/testfile1.mp4' })
-    expect(test.error).toEqual("ENOENT: no such file or directory, open './test/testfile1.mp4'")
+    expect(test.error).to.eql("ENOENT: no such file or directory, open './test/testfile1.mp4'")
   })
 
   it('Test without params', async () => {
     let test = await acfh.getHash()
-    expect(test.error).toEqual('noSourceSet')
+    expect(test.error).to.eql('noSourceSet')
   })
 
   it('Browser function - Test buffer from local file', async () => {
     const buffer = fs.readFileSync('./test/testfile.mp4')
     let test = await acfhBrowser.getHash({ buffer })
-    expect(test.type).toEqual('buffer')
-    expect(test.hash).toEqual(expectedHash)
-    expect(test.fileSize).toEqual(expectedFileSize)
+    expect(test.type).to.eql('buffer')
+    expect(test.hash).to.eql(expectedHash)
+    expect(test.fileSize).to.eql(expectedFileSize)
   })
 
   it('Browser function - Test URL', async () => {
     let test = await acfhBrowser.getHash({ url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' })
-    expect(test.type).toEqual('url')
-    expect(test.hash).toEqual(expectedHash)
-    expect(test.fileSize).toEqual(expectedFileSize)
+    expect(test.type).to.eql('url')
+    expect(test.hash).to.eql(expectedHash)
+    expect(test.fileSize).to.eql(expectedFileSize)
   })
 
   it('Browser function - Test non existing URL', async () => {
     let test = await acfhBrowser.getHash({ url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp3' })
-    expect(test.error).toEqual('invalidURL')
+    expect(test.error).to.eql('invalidURL')
   })
 
   it('Browser function - Test wrong buffer type', async () => {
     let test = await acfhBrowser.getHash({ arrayBuffer: '' })
-    expect(test.error).toEqual('noSourceSet')
+    expect(test.error).to.eql('noSourceSet')
   })
 
   it('Browser function - Test without params', async () => {
     let test = await acfhBrowser.getHash()
-    expect(test.error).toEqual('noSourceSet')
+    expect(test.error).to.eql('noSourceSet')
   })
 })
